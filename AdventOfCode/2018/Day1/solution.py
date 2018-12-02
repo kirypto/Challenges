5e52cacc16814ccb35405318b7a1a075bc1dcd08
kirypto <kirypto@gmail.com>
2018-12-01 18:57:00 -0600

    Day1:P1 * Added basic solution template

---
+++ AdventOfCode/2018/Day1/solution.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def main():
+    print("Main")
+    pass
+
+
+def tests():
+    print("Test")
+    pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise RuntimeError("Must provide run mode argument")
+    run_mode = sys.argv[1]
+    test_mode = "Test"
+    main_mode = "Main"
+    if run_mode == test_mode:
+        tests()
+    elif run_mode == main_mode:
+        main()
+    else:
+        raise RuntimeError(f"Run run_mode must be either '{test_mode}' or '{main_mode}', was {run_mode}")
