6353d8534334e8809763ea2b9c2115a5eecb6d53
kirypto <kirypto@gmail.com>
2018-12-05 20:00:17 -0600

    Day4:P1 * Added test cases

--- AdventOfCode/2018/Day4/part1.py
+++ AdventOfCode/2018/Day4/part1.py
@@ -2,7 +2,7 @@ import sys
 from typing import List
 
 
-def METHOD_NAME(INPUT: List[str]) -> None:
+def find_sneak_time(guard_log: List[str]) -> None:
     pass
 
 
@@ -10,14 +10,34 @@ def main():
     with open("input.txt", "r") as file:
         input_list = [line.strip() for line in file.readlines()]
 
-    result = METHOD_NAME(input_list)
+    result = find_sneak_time(input_list)
     print(f"Result: {result}")
 
 
 def tests():
-    test_cases = []
+    test_cases = [
+        ((240, 10, 24), [
+            "[1518-11-01 00:00] Guard #10 begins shift"
+            "[1518-11-01 00:05] falls asleep"
+            "[1518-11-01 00:25] wakes up"
+            "[1518-11-01 00:30] falls asleep"
+            "[1518-11-01 00:55] wakes up"
+            "[1518-11-01 23:58] Guard #99 begins shift"
+            "[1518-11-02 00:40] falls asleep"
+            "[1518-11-02 00:50] wakes up"
+            "[1518-11-03 00:05] Guard #10 begins shift"
+            "[1518-11-03 00:24] falls asleep"
+            "[1518-11-03 00:29] wakes up"
+            "[1518-11-04 00:02] Guard #99 begins shift"
+            "[1518-11-04 00:36] falls asleep"
+            "[1518-11-04 00:46] wakes up"
+            "[1518-11-05 00:03] Guard #99 begins shift"
+            "[1518-11-05 00:45] falls asleep"
+            "[1518-11-05 00:55] wakes up"
+        ])
+    ]
     for expected_output, test_input in test_cases:
-        output = METHOD_NAME(test_input)
+        output = find_sneak_time(test_input)
         if expected_output != output:
             print(ValueError(f"Expected {expected_output}, got {output}. Input was {test_input}"))
         else:
