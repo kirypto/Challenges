ccce7d62ffc719023d8caa1945da256e9d5cf063
kirypto <kirypto@gmail.com>
2018-12-06 14:11:53 -0600

    Day5:P1 * Added solution template

---
+++ AdventOfCode/2018/Day5/part1.py
@@ -0,0 +1,38 @@
+import sys
+from typing import List
+
+
+def METHOD_NAME(INPUT: List[str]) -> None:
+    pass
+
+
+def main():
+    with open("input.txt", "r") as file:
+        input_list = [line.strip() for line in file.readlines()]
+
+    result = METHOD_NAME(input_list)
+    print(f"Result: {result}")
+
+
+def tests():
+    test_cases = []
+    for expected_output, test_input in test_cases:
+        output = METHOD_NAME(test_input)
+        if expected_output != output:
+            print(ValueError(f"Expected {expected_output}, got {output}. Input was {test_input}"))
+        else:
+            print("Test passed!")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise RuntimeError("Must provide run mode argument")
+    run_mode = sys.argv[1]
+    test_mode = "Test"
+    main_mode = "Main"
+    if run_mode == test_mode:
+        tests()
+    elif run_mode == main_mode:
+        main()
+    else:
+        raise RuntimeError(f"Run run_mode must be either '{test_mode}' or '{main_mode}', was {run_mode}")
