b520bc00e54b6be68b2c79fa8b79cfdf2e63d558
kirypto <kirypto@gmail.com>
2018-12-06 14:44:24 -0600

    Day5:P1 * Implemented reduce_polymer

--- AdventOfCode/2018/Day5/part1.py
+++ AdventOfCode/2018/Day5/part1.py
@@ -1,9 +1,25 @@
+import re
 import sys
-from typing import Tuple
+from string import ascii_lowercase
+from typing import Tuple, Dict
 
 
 def reduce_polymer(polymer_string: str) -> Tuple[int, str]:
-    pass
+    # noinspection PyUnresolvedReferences
+    regs: Dict[str, re.Pattern] = {}
+    for char in ascii_lowercase:
+        regs[char] = re.compile(f"({char}{char.upper()}|{char.upper()}{char})")
+
+    prev_polymer_length = -1
+    while prev_polymer_length != len(polymer_string):
+        prev_polymer_length = len(polymer_string)
+        for char, regex in regs.items():
+            tmp_length = len(polymer_string)
+            polymer_string = regex.sub("", polymer_string)
+            remove_count = tmp_length - len(polymer_string)
+            if remove_count > 0:
+                print(f"Replaced { remove_count } characters!")
+    return len(polymer_string), polymer_string
 
 
 def main():
