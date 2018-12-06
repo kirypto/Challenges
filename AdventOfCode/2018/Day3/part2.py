3fc49e14b3e0a6279461e660307904abad8be418
kirypto <kirypto@gmail.com>
2018-12-05 19:03:53 -0600

    Day3:P2 * Copied part 1 for baseline

---
+++ AdventOfCode/2018/Day3/part2.py
@@ -0,0 +1,57 @@
+import re
+import sys
+from typing import List, Tuple
+
+import numpy as np
+
+
+def find_overlap(cuts: List[str]) -> int:
+
+    pattern = re.compile("#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+
+    def parse_cut(cut_str: str) -> Tuple[int, int, int, int, int]:
+        match = pattern.match(cut_str)
+        return int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)), int(match.group(5))
+
+    fabric = np.zeros((1000, 1000))
+
+    for cut in cuts:
+        cut_id, x, y, w, h = parse_cut(cut)
+        fabric[y:y+h, x:x+w] += 1
+
+    num_of_overlaps: int = np.sum(fabric > 1)
+    return num_of_overlaps
+
+
+def main():
+    with open("input.txt", "r") as file:
+        input_list = [line.strip() for line in file.readlines()]
+
+    result = find_overlap(input_list)
+    print(f"Result: {result}")
+
+
+def tests():
+    test_cases = [
+        (4, ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"])
+    ]
+    for expected_output, test_input in test_cases:
+        output = find_overlap(test_input)
+        if expected_output != output:
+            print(ValueError(f"Expected {expected_output}, got {output}. Input was {test_input}"))
+        else:
+            print("Test passed!")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise RuntimeError("Must provide run mode argument")
+    run_mode = sys.argv[1]
+    test_mode = "Test"
+    main_mode = "Main"
+    if run_mode == test_mode:
+        tests()
+    elif run_mode == main_mode:
+        main()
+    else:
+        raise RuntimeError(f"Run run_mode must be either '{test_mode}' or '{main_mode}', was {run_mode}")
