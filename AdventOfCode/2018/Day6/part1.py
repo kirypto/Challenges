e4154793fb8e16cdd1bf40b928cbe11b9e94f7a9
kirypto <kirypto@gmail.com>
2018-12-07 00:50:48 -0600

    Day6:P1 * Implemented step 1

--- AdventOfCode/2018/Day6/part1.py
+++ AdventOfCode/2018/Day6/part1.py
@@ -1,9 +1,70 @@
 import sys
+from itertools import chain
+from string import ascii_letters
 from typing import List, Tuple
+
+import numpy as np
+
+
+def manhattan_dist_mask(shape: Tuple[int, int], center: Tuple[int, int]):
+    shape_y, shape_x = shape
+    center_y, center_x = center
+
+    y, x = np.ogrid[-center_y: shape_y - center_y, -center_x: shape_x - center_x]
+
+    a = np.abs(y + x)
+    b = np.abs(y - x)
+    quadrant_selector = np.array([[
+        x < center_x and y < center_y or x > center_x and y > center_y
+        for x in range(shape_x)
+    ]
+        for y in range(shape_y)
+    ])
+    return np.where(quadrant_selector, a, b)
 
 
 def find_largest_finite_area(origins: List[str]) -> Tuple[int, str, List[str]]:
-    pass
+    origins = [(int(y), int(x)) for x, y in [coord.split(", ") for coord in origins]]
+    max_y: int = max([coord[0] for coord in origins])
+    max_x: int = max([coord[1] for coord in origins])
+
+    closest_map = np.zeros((len(origins), max_y + 1, max_x + 1))
+
+    for index, coord in enumerate(origins):
+        y, x = coord
+        manhattan_dist_from_coord = manhattan_dist_mask((max_y + 1, max_x + 1), (y, x))
+        weighting = np.abs(manhattan_dist_from_coord - (max_x + max_y))
+        closest_map[index] += weighting
+
+    closest_index = np.argmax(closest_map, axis=0)
+
+    max_values = np.take_along_axis(closest_map, np.expand_dims(closest_index, axis=0), axis=0)
+    indices_where_multiple_are_max = np.sum(closest_map == max_values, axis=0) > 1
+
+    dot_index = len(origins)
+    result = np.where(indices_where_multiple_are_max, dot_index, closest_index)
+
+    letter_chain = chain(ascii_letters)
+    letters = [next(letter_chain) for i in range(len(origins))]
+    letters.append(".")
+    letters = np.array(letters)
+
+    letter_map = letters[result]
+    edge_selector = np.zeros((max_y - 1, max_x - 1)).astype(np.dtype("bool"))
+    edge_selector = np.pad(edge_selector, 1, mode="constant", constant_values=True)
+    disallowed = sorted(list(set(letter_map[edge_selector])))
+    disallowed.remove(".")
+    largest_area = -1
+    largest_letter = None
+    for letter in set(letter_map.flatten()):
+        print(letter)
+        if letter == "." or letter in disallowed:
+            continue
+        area_of_letter = np.sum(letter_map == letter)
+        if area_of_letter > largest_area:
+            largest_area = area_of_letter
+            largest_letter = letter
+    return largest_area, largest_letter, disallowed
 
 
 def main():
