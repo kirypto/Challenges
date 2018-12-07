21a3faf79f5eb4c6af5cb33d374ad16d74a59da9
kirypto <kirypto@gmail.com>
2018-12-06 21:56:25 -0600

    Day6:P1 * Tweaked test setup

--- AdventOfCode/2018/Day6/part1.py
+++ AdventOfCode/2018/Day6/part1.py
@@ -1,8 +1,8 @@
 import sys
-from typing import List
+from typing import List, Tuple
 
 
-def METHOD_NAME(INPUT: List[str]) -> None:
+def find_largest_finite_area(origins: List[str]) -> Tuple[int, str, List[str]]:
     pass
 
 
@@ -10,14 +10,23 @@ def main():
     with open("input.txt", "r") as file:
         input_list = [line.strip() for line in file.readlines()]
 
-    result = METHOD_NAME(input_list)
+    result = find_largest_finite_area(input_list)
     print(f"Result: {result}")
 
 
 def tests():
-    test_cases = []
+    test_cases = [
+        ((17, "e", ["a", "b", "c", "f"]), [
+            "1, 1",
+            "1, 6",
+            "8, 3",
+            "3, 4",
+            "5, 5",
+            "8, 9",
+        ])
+    ]
     for expected_output, test_input in test_cases:
-        output = METHOD_NAME(test_input)
+        output = find_largest_finite_area(test_input)
         if expected_output != output:
             print(ValueError(f"Expected {expected_output}, got {output}. Input was {test_input}"))
         else:
