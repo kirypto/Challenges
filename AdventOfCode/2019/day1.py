66d93b923dd1ea83f22309ca2959de13e0ea7462
kirypto <kirypto@gmail.com>
2019-12-02 12:46:47 -0600

    AdventOfCode2019 * Added tests and placeholder method for day 1 solution

---
+++ AdventOfCode/2019/day1.py
@@ -0,0 +1,34 @@
+from typing import Callable, Any, List, Tuple
+
+
+def calc_fuel(mass: int) -> int:
+    return -1
+
+
+def do_tests(method_under_test: Callable[[Any], Any], test_cases: List[Tuple[Any, Any]]):
+    for expected_output, test_input in test_cases:
+        output = method_under_test(test_input)
+        if expected_output != output:
+            print(ValueError(f"Expected {expected_output}, got {output}. Input was {test_input}"))
+        else:
+            print("Test passed!")
+
+
+def tests():
+    test_cases = [
+        (2, 12),
+        (2, 14),
+        (654, 1969),
+        (33583, 100756),
+    ]
+    method_under_test = calc_fuel
+    do_tests(method_under_test, test_cases)
+    print("", flush=True)
+
+
+def _main():
+    pass
+
+
+if __name__ == '__main__':
+    _main()
