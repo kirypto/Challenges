30d25ae79da8c8b2455a76baaaef312487837acf
kirypto <kirypto@gmail.com>
2019-12-02 13:01:24 -0600

    AdventOfCode2019 * Added functionality to read puzzle input and completed day 1 part 1

--- AdventOfCode/2019/day1.py
+++ AdventOfCode/2019/day1.py
@@ -5,6 +5,13 @@ def calc_fuel(mass: int) -> int:
     return mass // 3 - 2
 
 
+def read_puzzle_input():
+    input_file_path = str(__file__).replace(".py", "_input.txt")
+    with open(input_file_path, mode="r") as input_file:
+        puzzle_input = input_file.readlines()
+    return puzzle_input
+
+
 def do_tests(method_under_test: Callable[[Any], Any], test_cases: List[Tuple[Any, Any]]):
     for expected_output, test_input in test_cases:
         output = method_under_test(test_input)
@@ -27,7 +34,8 @@ def tests():
 
 
 def _main():
-    pass
+    puzzle_input = read_puzzle_input()
+    print(sum([calc_fuel(int(x)) for x in puzzle_input]))
 
 
 if __name__ == '__main__':
