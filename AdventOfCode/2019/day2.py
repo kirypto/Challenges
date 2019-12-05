cf0d743e46e5afa81adcf908add266d4b22115a0
kirypto <kirypto@gmail.com>
2019-12-04 21:50:35 -0600

    AdventOfCode2019 * Implemented the day2 input translation

--- AdventOfCode/2019/day2.py
+++ AdventOfCode/2019/day2.py
@@ -1,4 +1,10 @@
+from typing import List
+
 from python_tools.advent_of_code_lib import AdventOfCodeProblem
+
+
+def translate_input(raw: str) -> List[int]:
+    return [int(x) for x in raw.split(",")]
 
 
 problem = AdventOfCodeProblem(
@@ -6,5 +12,5 @@ problem = AdventOfCodeProblem(
     lambda x: x,
     [],
     lambda x: x,
-    lambda x: x
+    translate_input
 )
