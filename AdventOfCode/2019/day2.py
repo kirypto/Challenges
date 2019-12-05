e5f83759bc19084b127328558c307e1b32eef087
kirypto <kirypto@gmail.com>
2019-12-05 15:43:29 -0600

    AdventOfCode2019 * Implemented day 2 solution

--- AdventOfCode/2019/day2.py
+++ AdventOfCode/2019/day2.py
@@ -3,8 +3,34 @@ from typing import List
 from python_tools.advent_of_code_lib import AdventOfCodeProblem, TestCase
 
 
+def run_program(input_program: List[int]) -> List[int]:
+    program = list(input_program)
+    program_position = 0
+    while True:
+        op_code = program[program_position]
+        if op_code == 99:
+            break
+        input_a_position = program[program_position + 1]
+        input_b_position = program[program_position + 2]
+        input_a = program[input_a_position]
+        input_b = program[input_b_position]
+        output_position = program[program_position + 3]
+        if op_code == 1:
+            def operator(a, b): return a + b
+        elif op_code == 2:
+            def operator(a, b): return a * b
+        else:
+            raise ValueError(f"OpCode {op_code} not supported")
+        program[output_position] = operator(input_a, input_b)
+        program_position += 4
+    return program
+
+
 def translate_input(raw: str) -> List[int]:
-    return [int(x) for x in raw.split(",")]
+    int_input_list = [int(x) for x in raw.split(",")]
+    int_input_list[1] = 12
+    int_input_list[2] = 2
+    return int_input_list
 
 
 part_1_test_cases = [
@@ -16,7 +42,7 @@ part_1_test_cases = [
 
 problem = AdventOfCodeProblem(
     part_1_test_cases,
-    lambda x: x,
+    run_program,
     [],
     lambda x: x,
     translate_input
