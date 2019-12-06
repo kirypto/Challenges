fcbbdeb266a87565b8611557590e2b27c06bfcaa
kirypto <kirypto@gmail.com>
2019-12-05 18:52:24 -0600

    AdventOfCode2019 * Added puzzle template

---
+++ AdventOfCode/2019/template.py
@@ -0,0 +1,18 @@
+from python_tools.advent_of_code_lib import AdventOfCodeProblem, TestCase
+
+
+part_1_test_cases = [
+    TestCase(None, None)
+]
+
+part_2_test_cases = [
+    TestCase(None, None)
+]
+
+problem = AdventOfCodeProblem(
+    part_1_test_cases,
+    lambda x: x,
+    part_2_test_cases,
+    lambda x: x,
+    lambda x: x
+)
