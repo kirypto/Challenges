2fb9540ea719225c4cc5015cd722f16b3372d498
kirypto <kirypto@gmail.com>
2019-12-07 17:00:38 -0600

    AdventOfCode2019 * Tweaked day 2 part 1 implementation for supporting future computer use

--- AdventOfCode/2019/day2.py
+++ AdventOfCode/2019/day2.py
@@ -1,31 +1,39 @@
+from enum import Enum
 from typing import List
 
 from python_tools.advent_of_code_lib import AdventOfCodeProblem, TestCase
 
 
-def run_program(input_program: List[int]) -> List[int]:
-    program = list(input_program)
-    program_position = 0
+class OpCode(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    HALT = 99
+
+
+# noinspection SpellCheckingInspection
+def run_intcode_program(input_program: List[int]) -> List[int]:
+    program_memory = list(input_program)
+    instruction_pointer = 0
     while True:
-        op_code = program[program_position]
-        if op_code == 99:
+        op_code = OpCode(program_memory[instruction_pointer])
+        if op_code == OpCode.HALT:
             break
-        input_a_position = program[program_position + 1]
-        input_b_position = program[program_position + 2]
-        input_a = program[input_a_position]
-        input_b = program[input_b_position]
-        output_position = program[program_position + 3]
-        if op_code == 1:
+        input_a_position = program_memory[instruction_pointer + 1]
+        input_b_position = program_memory[instruction_pointer + 2]
+        input_a = program_memory[input_a_position]
+        input_b = program_memory[input_b_position]
+        output_position = program_memory[instruction_pointer + 3]
+        if op_code == OpCode.ADD:
             def operator(a, b):
                 return a + b
-        elif op_code == 2:
+        elif op_code == OpCode.MULTIPLY:
             def operator(a, b):
                 return a * b
         else:
             raise ValueError(f"OpCode {op_code} not supported")
-        program[output_position] = operator(input_a, input_b)
-        program_position += 4
-    return program
+        program_memory[output_position] = operator(input_a, input_b)
+        instruction_pointer += 4
+    return program_memory
 
 
 def translate_input(raw: str) -> List[int]:
@@ -42,10 +50,12 @@ part_1_test_cases = [
     TestCase([30, 1, 1, 4, 2, 5, 6, 0, 99], [1, 1, 1, 4, 99, 5, 6, 0, 99])
 ]
 
+part_2_test_cases = part_1_test_cases  # No change in computer, just looking for magical combination
+
 problem = AdventOfCodeProblem(
     part_1_test_cases,
-    run_program,
-    [],
-    lambda x: x,
+    run_intcode_program,
+    part_2_test_cases,
+    run_intcode_program,
     translate_input
 )
