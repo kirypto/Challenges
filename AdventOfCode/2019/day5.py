a97eebef622199a62907305330ca3dc991e219a4
kirypto <kirypto@gmail.com>
2019-12-08 17:42:26 -0600

    AdventOfCode2019 * Implemented day 5 part 1

--- AdventOfCode/2019/day5.py
+++ AdventOfCode/2019/day5.py
@@ -1,22 +1,24 @@
-from typing import Any
+from typing import Any, List
 
 from python_tools.advent_of_code.puzzle_runner_helpers import AdventOfCodeProblem, TestCase
+from python_tools.advent_of_code.y2019_int_code_computer import run_int_code_program
 
 
-def part_1_solver(puzzle_input: Any) -> Any:
-    return puzzle_input
+def part_1_solver(puzzle_input: List[int]) -> List[int]:
+    result = run_int_code_program(puzzle_input)
+    return result
 
 
 def part_2_solver(puzzle_input: Any) -> Any:
     return puzzle_input
 
 
-def translate_input(puzzle_input_raw: str, part_num: int) -> Any:
-    return puzzle_input_raw
+def translate_input(puzzle_input_raw: str, part_num: int) -> List[int]:
+    return [int(x) for x in puzzle_input_raw.split(",")]
 
 
 part_1_test_cases = [
-    TestCase(None, None)
+    TestCase([6, 0, 4, 0, 99], [3, 0, 4, 0, 99])
 ]
 
 part_2_test_cases = [
@@ -28,6 +30,5 @@ problem = AdventOfCodeProblem(
     part_1_solver,
     part_2_test_cases,
     part_2_solver,
-    translate_input,
-    run_tests_only=True
+    translate_input
 )
