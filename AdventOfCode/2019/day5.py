f06e712ad26ee19a395f038542bb5531250a4983
kirypto <kirypto@gmail.com>
2019-12-09 12:48:09 -0600

    AdventOfCode2019 * Added tests for day 5 less than and equals operations

--- AdventOfCode/2019/day5.py
+++ AdventOfCode/2019/day5.py
@@ -10,7 +10,8 @@ def part_1_solver(puzzle_input: List[int]) -> List[int]:
 
 
 def part_2_solver(puzzle_input: Any) -> Any:
-    return puzzle_input
+    result = run_int_code_program(puzzle_input)
+    return result
 
 
 def translate_input(puzzle_input_raw: str, part_num: int) -> List[int]:
@@ -18,11 +19,18 @@ def translate_input(puzzle_input_raw: str, part_num: int) -> List[int]:
 
 
 part_1_test_cases = [
-    TestCase([6, 0, 4, 0, 99], [3, 0, 4, 0, 99])
+    TestCase([6, 0, 4, 0, 99], [3, 0, 4, 0, 99])  # Enter 6 when asked, should set position 0 to 6
 ]
 
 part_2_test_cases = [
-    TestCase(None, None)
+    # Enter 7 when asked, should set position 9 to 1 because 7 is less than 8
+    TestCase([3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8], [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]),
+    # Enter 8 when asked, should set position 9 to 1 because 8 equals 8
+    TestCase([3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8], [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]),
+    # Enter 8 when asked, should set position 3 to 0 because 8 is NOT less than 8
+    TestCase([3, 3, 1108, 1, 8, 3, 4, 3, 99], [3, 3, 1108, -1, 8, 3, 4, 3, 99]),
+    # Enter 7 when asked, should set position 3 to 0 because 7 is NOT equal to 8
+    TestCase([3, 3, 1107, 1, 8, 3, 4, 3, 99], [3, 3, 1107, -1, 8, 3, 4, 3, 99]),
 ]
 
 problem = AdventOfCodeProblem(
@@ -30,5 +38,6 @@ problem = AdventOfCodeProblem(
     part_1_solver,
     part_2_test_cases,
     part_2_solver,
-    translate_input
+    translate_input,
+    run_tests_only=True
 )
