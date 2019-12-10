71e8f18a7a7dbfd2fcdda9f12b842280aefe1f81
kirypto <kirypto@gmail.com>
2019-12-09 21:14:26 -0600

    AdventOfCode2019 * Tied in buffered input and output for day 5 tests

--- AdventOfCode/2019/day5.py
+++ AdventOfCode/2019/day5.py
@@ -1,36 +1,48 @@
-from typing import Any, List
+from typing import List, Tuple
 
 from python_tools.advent_of_code.puzzle_runner_helpers import AdventOfCodeProblem, TestCase
-from python_tools.advent_of_code.y2019_int_code_computer import run_int_code_program
+from python_tools.advent_of_code.y2019_int_code_computer import run_int_code_program, add_buffered_input, get_buffered_output
 
 
-def part_1_solver(puzzle_input: List[int]) -> List[int]:
-    result = run_int_code_program(puzzle_input)
-    return result
+def part_1_solver(puzzle_input: Tuple[List[int], List[int]]) -> List[int]:
+    program, to_be_buffered = puzzle_input
+    for val in to_be_buffered:
+        add_buffered_input(val)
+    run_int_code_program(program)
+    program_output = get_buffered_output()
+    return program_output
 
 
-def part_2_solver(puzzle_input: Any) -> Any:
-    result = run_int_code_program(puzzle_input)
-    return result
+def part_2_solver(puzzle_input:Tuple[List[int], List[int]]) -> List[int]:
+    program, to_be_buffered = puzzle_input
+    for val in to_be_buffered:
+        add_buffered_input(val)
+    run_int_code_program(program)
+    program_output = get_buffered_output()
+    return program_output
 
 
-def translate_input(puzzle_input_raw: str, part_num: int) -> List[int]:
-    return [int(x) for x in puzzle_input_raw.split(",")]
+def translate_input(puzzle_input_raw: str, part_num: int) -> Tuple[List[int], List[int]]:
+    if part_num == 1:
+        return [int(x) for x in puzzle_input_raw.split(",")], [1]
+    elif part_num == 2:
+        return [int(x) for x in puzzle_input_raw.split(",")], [5]
 
 
 part_1_test_cases = [
-    TestCase([6, 0, 4, 0, 99], [3, 0, 4, 0, 99])  # Enter 6 when asked, should set position 0 to 6
+    TestCase([6], ([3, 0, 4, 0, 99], [6])),  # 6 is buffered for input, program reads number in and outputs it
+    TestCase([8], ([3, 0, 4, 0, 99], [8])),  # 8 is buffered for input, program reads number in and outputs it
 ]
 
 part_2_test_cases = [
-    # Enter 7 when asked, should set position 9 to 1 because 7 is less than 8
-    TestCase([3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8], [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]),
-    # Enter 8 when asked, should set position 9 to 1 because 8 equals 8
-    TestCase([3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8], [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]),
-    # Enter 8 when asked, should set position 3 to 0 because 8 is NOT less than 8
-    TestCase([3, 3, 1108, 1, 8, 3, 4, 3, 99], [3, 3, 1108, -1, 8, 3, 4, 3, 99]),
-    # Enter 7 when asked, should set position 3 to 0 because 7 is NOT equal to 8
-    TestCase([3, 3, 1107, 1, 8, 3, 4, 3, 99], [3, 3, 1107, -1, 8, 3, 4, 3, 99]),
+    TestCase([0], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [7])),
+    TestCase([1], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])),
+    TestCase([1], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [7])),
+    TestCase([0], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [8])),
+    TestCase([0], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [7])),
+    TestCase([1], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [8])),
+    TestCase([1], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [7])),
+    TestCase([0], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [8])),
 ]
 
 problem = AdventOfCodeProblem(
