ce04ce7002b7fad243a359f982b6f72adc37a4a8
kirypto <kirypto@gmail.com>
2019-12-09 21:24:30 -0600

    AdventOfCode2019 * Flushed out day 5 part 2 tests

--- AdventOfCode/2019/day5.py
+++ AdventOfCode/2019/day5.py
@@ -13,7 +13,7 @@ def part_1_solver(puzzle_input: Tuple[List[int], List[int]]) -> List[int]:
     return program_output
 
 
-def part_2_solver(puzzle_input:Tuple[List[int], List[int]]) -> List[int]:
+def part_2_solver(puzzle_input: Tuple[List[int], List[int]]) -> List[int]:
     program, to_be_buffered = puzzle_input
     for val in to_be_buffered:
         add_buffered_input(val)
@@ -35,14 +35,24 @@ part_1_test_cases = [
 ]
 
 part_2_test_cases = [
-    TestCase([0], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [7])),
-    TestCase([1], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])),
-    TestCase([1], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [7])),
-    TestCase([0], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [8])),
-    TestCase([0], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [7])),
-    TestCase([1], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [8])),
-    TestCase([1], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [7])),
-    TestCase([0], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [8])),
+    TestCase([0], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [7])),  # Position mode eq test
+    TestCase([1], ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])),  # Position mode eq test
+    TestCase([1], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [7])),  # Position mode less than test
+    TestCase([0], ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [8])),  # Position mode less than test
+    TestCase([0], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [7])),  # Immediate mode eq test
+    TestCase([1], ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [8])),  # Immediate mode eq test
+    TestCase([1], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [7])),  # Immediate mode less than test
+    TestCase([0], ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [8])),  # Immediate mode less than test
+    TestCase([0], ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [0])),  # Position mode jump test
+    TestCase([1], ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [745])),  # Position mode jump test
+    TestCase([0], ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [0])),  # Immediate mode jump test
+    TestCase([1], ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [2341])),  # Immediate mode jump test
+    TestCase([999], ([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1,
+                      46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [7])),  # Outputs 999 if input < 8
+    TestCase([1000], ([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1,
+                       46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [8])),  # Outputs 1000 if input == 8
+    TestCase([1001], ([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1,
+                       46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [9])),  # Outputs 1001 if input > 8
 ]
 
 problem = AdventOfCodeProblem(
