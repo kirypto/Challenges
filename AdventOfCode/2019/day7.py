598eee9c412c36b181333d7f4cc05a17108cc07c
kirypto <kirypto@gmail.com>
2019-12-13 07:00:49 -0600

    AdventOfCode2019 * Tweaked displayed output for day 7

--- AdventOfCode/2019/day7.py
+++ AdventOfCode/2019/day7.py
@@ -1,11 +1,11 @@
 from itertools import permutations
-from typing import Any, List, Optional
+from typing import List, Optional, Tuple
 
 from python_tools.advent_of_code.puzzle_runner_helpers import AdventOfCodeProblem, TestCase
 from python_tools.advent_of_code.y2019_int_code_computer import add_buffered_input, run_int_code_program, get_buffered_output, set_verbose
 
 
-def part_1_solver(puzzle_input: List[int]) -> List[int]:
+def part_1_solver(puzzle_input: List[int]) -> Tuple[int, List[int]]:
     set_verbose(False)
     all_possible_phase_settings = list(permutations(range(5)))
     max_phase_setting: List[int] = []
@@ -25,23 +25,22 @@ def part_1_solver(puzzle_input: List[int]) -> List[int]:
         if max_thruster_signal is None or output > max_thruster_signal:
             max_phase_setting = phase_setting
             max_thruster_signal = output
-    print(f"Thruster signal: {max_thruster_signal}, phase setting: {max_phase_setting}")
-    return max_phase_setting
+    return max_thruster_signal, max_phase_setting
 
 
 def part_2_solver(puzzle_input: List[int]) -> List[int]:
     return puzzle_input
 
 
-def translate_input(puzzle_input_raw: str, part_num: int) -> Any:
-    return puzzle_input_raw
+def translate_input(puzzle_input_raw: str, _: int) -> List[int]:
+    return [int(x) for x in puzzle_input_raw.split(",")]
 
 
 part_1_test_cases = [
-    TestCase([4, 3, 2, 1, 0], [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]),
-    TestCase([0, 1, 2, 3, 4], [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]),
-    TestCase([1, 0, 4, 3, 2], [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4,
-                               31, 99, 0, 0, 0]),
+    TestCase((43210, [4, 3, 2, 1, 0]), [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]),
+    TestCase((54321, [0, 1, 2, 3, 4]), [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]),
+    TestCase((65210, [1, 0, 4, 3, 2]), [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31,
+                                        31, 4, 31, 99, 0, 0, 0]),
 ]
 
 part_2_test_cases = [
@@ -54,5 +53,4 @@ problem = AdventOfCodeProblem(
     part_2_test_cases,
     part_2_solver,
     translate_input,
-    run_tests_only=True
 )
