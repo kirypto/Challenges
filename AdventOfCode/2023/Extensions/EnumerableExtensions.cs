8fd8981c0b86590c35192a8326f563ede122fafe
kirypto <kirypto@gmail.com>
2023-12-01 16:51:00 -0600

    AOC-2023 Add ForEach util function

---
+++ AdventOfCode/2023/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace kirypto.AdventOfCode._2023.Extensions; 
+
+public static class EnumerableExtensions {
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action) {
+        foreach (T t in enumerable) {
+            action(t);
+        }
+    }
+}
