ef23f52152582c417aaca3151c5a9930a222cf96
kirypto <kirypto@gmail.com>
2023-12-01 17:42:17 -0600

    AOC-2023 Add Reversed extension method for strings

---
+++ AdventOfCode/2023/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace kirypto.AdventOfCode._2023.Extensions; 
+
+public static class StringExtensions {
+    public static string Reversed(this string s) {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
