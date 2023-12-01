055fe9cf2a086a890e146f8ccde9ec087cdb41d0
kirypto <kirypto@gmail.com>
2023-12-01 17:01:13 -0600

    AOC-2023 Solve day 1 sample

--- AdventOfCode/2023/Program.cs
+++ AdventOfCode/2023/Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Text.RegularExpressions;
+using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
 
 Console.WriteLine("Hello, World!");
 
 IInputRepository inputRepository = new FileInputRepository();
 
+int ExtractCalibrationValue(string raw) {
+    Match match = Regex.Match(raw, @"^[^\d]*(\d).*?(\d)?[^\d]*$");
+    string secondDigit = match.Groups[2].Length > 0 ? match.Groups[2].Value : match.Groups[1].Value;
+    return int.Parse($"{match.Groups[1]}{secondDigit}");
+}
 
-Console.WriteLine(inputRepository.Fetch("day1-1"));
+
+int calibrationSum = inputRepository.FetchLines("day1-1")
+        .Select(ExtractCalibrationValue)
+        .Sum();
+Console.WriteLine($"Calibration sum: {calibrationSum}");
