4596bdaef54733be30d2d77bf2bf584f4efd57aa
kirypto <kirypto@gmail.com>
2023-12-01 17:12:09 -0600

    AOC-2023 Solve day 1

--- AdventOfCode/2023/Program.cs
+++ AdventOfCode/2023/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System.Text.RegularExpressions;
 using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
 
@@ -10,10 +9,39 @@ Console.WriteLine("Hello, World!");
 IInputRepository inputRepository = new FileInputRepository();
 
 int ExtractCalibrationValue(string raw) {
-    Match match = Regex.Match(raw, @"^[^\d]*(\d).*?(\d)?[^\d]*$");
-    string secondDigit = match.Groups[2].Length > 0 ? match.Groups[2].Value : match.Groups[1].Value;
+    var replacements = new List<CalibrationDigit> {
+            new("0", "0"),
+            new("1", "1"),
+            new("2", "2"),
+            new("3", "3"),
+            new("4", "4"),
+            new("5", "5"),
+            new("6", "6"),
+            new("7", "7"),
+            new("8", "8"),
+            new("9", "9"),
+            new("zero", "0"),
+            new("one", "1"),
+            new("two", "2"),
+            new("three", "3"),
+            new("four", "4"),
+            new("five", "5"),
+            new("six", "6"),
+            new("seven", "7"),
+            new("eight", "8"),
+            new("nine", "9"),
+    };
     try {
-        return int.Parse($"{match.Groups[1]}{secondDigit}");
+        string mutated = raw;
+        string? firstDigit = replacements
+                .Where(r => mutated.Contains(r.Raw))
+                .MinBy(r => mutated.IndexOf(r.Raw, StringComparison.Ordinal))
+                .Normalized;
+        string? secondDigit = replacements
+                .Where(r => mutated.Reversed().Contains(r.Raw.Reversed()))
+                .MinBy(r => mutated.Reversed().IndexOf(r.Raw.Reversed(), StringComparison.Ordinal))
+                .Normalized;
+        return int.Parse(firstDigit + secondDigit);
     }
     catch (Exception e) {
         Console.WriteLine($"Failed on value '{raw}'");
@@ -23,7 +51,10 @@ int ExtractCalibrationValue(string raw) {
 }
 
 
-int calibrationSum = inputRepository.FetchLines("day1-1")
+int calibrationSum = inputRepository.FetchLines("day1-real")
         .Select(ExtractCalibrationValue)
         .Sum();
 Console.WriteLine($"Calibration sum: {calibrationSum}");
+
+
+internal readonly record struct CalibrationDigit(string Raw, string Normalized);
