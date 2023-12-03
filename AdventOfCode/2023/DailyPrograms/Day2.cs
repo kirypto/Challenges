8d1284f7ea39632f25d7a62a83b38ac62d15364f
kirypto <kirypto@gmail.com>
2023-12-02 23:26:24 -0600

    AOC-2023 Finish day 2 part 2

--- AdventOfCode/2023/DailyPrograms/Day2.cs
+++ AdventOfCode/2023/DailyPrograms/Day2.cs
@@ -12,30 +12,43 @@ public class Day2 : IDailyProgram {
             int gameId = int.Parse(gameIdAndDraws[0]);
             string allDraws = gameIdAndDraws[1];
 
-            if (AreDrawsPossible(allDraws)) {
-                sum += gameId;
-            }
-        }
-        Console.WriteLine($"Sum of possible game IDs: {sum}");
-    }
+            IList<string> draws = allDraws.Split("; ");
 
-    private static bool AreDrawsPossible(string allDraws) {
-        IList<string> draws = allDraws.Split("; ");
 
-        foreach (string draw in draws) {
-            foreach (string drawCount in draw.Split(", ")) {
-                string[] s = drawCount.Split(" ");
-                int count = int.Parse(s[0]);
-                string colour = s[1];
-                if (_maxCounts.ContainsKey(colour) && _maxCounts[colour] < count) {
-                    return false;
+            var maxObservedCounts = new Dictionary<string, int>();
+            var gameIsPossible = true;
+            foreach (string draw in draws) {
+                foreach (string drawCount in draw.Split(", ")) {
+                    string[] s = drawCount.Split(" ");
+                    int count = int.Parse(s[0]);
+                    string colour = s[1];
+                    if (_p1MaxCounts.ContainsKey(colour) && _p1MaxCounts[colour] < count) {
+                        gameIsPossible = false;
+                    }
+                    if (!maxObservedCounts.ContainsKey(colour)) {
+                        maxObservedCounts[colour] = count;
+                    } else {
+                        maxObservedCounts[colour] = Math.Max(maxObservedCounts[colour], count);
+                    }
                 }
             }
+
+            if (part == 1 && gameIsPossible) {
+                sum += gameId;
+            } else if (part == 2) {
+                int cubePower = maxObservedCounts.Values.Aggregate((c1, c2) => c1 * c2);
+                // Console.WriteLine($"Game {gameId} cube power: {cubePower}");
+                sum += cubePower;
+            }
         }
-        return true;
+        if (part == 1) {
+            Console.WriteLine($"Sum of possible game IDs: {sum}");
+        } else if (part == 2) {
+            Console.WriteLine($"Sum of possible cube power: {sum}");
+        }
     }
 
-    private static readonly IDictionary<string, int> _maxCounts = new Dictionary<string, int> {
+    private static readonly IDictionary<string, int> _p1MaxCounts = new Dictionary<string, int> {
             { "red", 12 },
             { "green", 13 },
             { "blue", 14 },
