317011de3417ffdd7d62d3b7270ff95f433fc983
kirypto <kirypto@gmail.com>
2023-12-02 23:04:46 -0600

    AOC-2023 Finish day 2 part 1

--- AdventOfCode/2023/DailyPrograms/Day2.cs
+++ AdventOfCode/2023/DailyPrograms/Day2.cs
@@ -1,20 +1,43 @@
-﻿using System.Text.RegularExpressions;
-using kirypto.AdventOfCode._2023.Extensions;
-using kirypto.AdventOfCode._2023.Repos;
+﻿using kirypto.AdventOfCode._2023.Repos;
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day2 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef) {
-        Console.Write("Input: ");
-        // string inputRef = "day2-" + Console.ReadLine()!;
-        inputRepository.FetchLines(inputRef)
-                .Select(line => line.Replace(";", ", ;") + ";")
-                .Select(line => {
-                    Console.WriteLine(line);
-                    return line;
-                })
-                .Select(game => Regex.Match(game, @"^Game (\d+): ((\d+ \w+, )+;)+"))
-                .ForEach(match => { Console.WriteLine(match.Groups.Count); });
+        IList<string> lines = inputRepository.FetchLines(inputRef);
+        var sum = 0;
+        foreach (string line in lines) {
+            string l = line.TrimStart("Game ".ToCharArray());
+            IList<string> gameIdAndDraws = l.Split(": ");
+            int gameId = int.Parse(gameIdAndDraws[0]);
+            string allDraws = gameIdAndDraws[1];
+
+            if (AreDrawsPossible(allDraws)) {
+                sum += gameId;
+            }
+        }
+        Console.WriteLine($"Sum of possible game IDs: {sum}");
     }
+
+    private static bool AreDrawsPossible(string allDraws) {
+        IList<string> draws = allDraws.Split("; ");
+
+        foreach (string draw in draws) {
+            foreach (string drawCount in draw.Split(", ")) {
+                string[] s = drawCount.Split(" ");
+                int count = int.Parse(s[0]);
+                string colour = s[1];
+                if (_maxCounts.ContainsKey(colour) && _maxCounts[colour] < count) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static readonly IDictionary<string, int> _maxCounts = new Dictionary<string, int> {
+            { "red", 12 },
+            { "green", 13 },
+            { "blue", 14 },
+    };
 }
