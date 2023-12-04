c983b8c3c587bfc9fc7c3172c1b2e69b078f34ae
kirypto <kirypto@gmail.com>
2023-12-04 07:01:05 -0600

    AOC-2023 Finish day 4 part 1

--- AdventOfCode/2023/DailyPrograms/Day4.cs
+++ AdventOfCode/2023/DailyPrograms/Day4.cs
@@ -1,9 +1,32 @@
-﻿using kirypto.AdventOfCode._2023.Repos;
+﻿using System.Text.RegularExpressions;
+using kirypto.AdventOfCode._2023.Repos;
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day4 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        throw new NotImplementedException();
+        double sum = inputRepository.FetchLines(inputRef)
+                .Select(line => line.Replace("Card ", ""))
+                .Select(line => Regex.Replace(line, @"\s+", " "))
+                .Select(line => {
+                    string[] gameIdAndRest = line.Split(": ");
+                    string[] winningAndReceived = gameIdAndRest[1].Split(" | ");
+                    ISet<int> winning = winningAndReceived[0].Split(" ")
+                            .Select(int.Parse)
+                            .ToHashSet();
+                    ISet<int> received = winningAndReceived[1].Split(" ")
+                            .Select(int.Parse)
+                            .ToHashSet();
+                    return new {
+                            CardNumber = int.Parse(gameIdAndRest[0]),
+                            Winning = winning,
+                            Received = received,
+                    };
+                })
+                .Select(obj => obj.Winning.Intersect(obj.Received).Count())
+                .Where(winningCount => winningCount > 0)
+                .Select(winningCount => Math.Pow(2, winningCount - 1))
+                .Sum();
+        Console.WriteLine($"Total winning points: {sum}");
     }
 }
