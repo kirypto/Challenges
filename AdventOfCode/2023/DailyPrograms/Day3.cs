05985bcaec042174c5ecff3c797f3ad87361a847
kirypto <kirypto@gmail.com>
2023-12-03 18:12:19 -0600

    AOC-2023 Move Array2dExtensions to own file

--- AdventOfCode/2023/DailyPrograms/Day3.cs
+++ AdventOfCode/2023/DailyPrograms/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
@@ -52,28 +53,5 @@ public class Day3 : IDailyProgram {
             }
         }
         return false;
-    }
-}
-
-public static class Array2dExtensions {
-    public delegate R Mask<in T, out R>(T[,] t, int row, int col);
-
-    public static R[,] ApplyMask<T, R>(this T[,] array, Mask<T, R> mask) {
-        var result = new R[array.GetLength(0), array.GetLength(1)];
-        for (var row = 0; row < array.GetLength(0); row++) {
-            for (var col = 0; col < array.GetLength(1); col++) {
-                result[row, col] = mask(array, row, col);
-            }
-        }
-        return result;
-    }
-
-    public static void PrintToConsole<T>(this T[,] array) {
-        for (var i = 0; i < array.GetLength(0); i++) {
-            for (var j = 0; j < array.GetLength(1); j++) {
-                Console.Write($"{array[i, j]} ");
-            }
-            Console.WriteLine();
-        }
     }
 }
