16dfe298a8446c21091c978415a87d3549cd1e80
kirypto <kirypto@gmail.com>
2023-12-04 07:10:00 -0600

    AOC-2023 Cleanup and prep for step 2

--- AdventOfCode/2023/DailyPrograms/Day4.cs
+++ AdventOfCode/2023/DailyPrograms/Day4.cs
@@ -5,7 +5,7 @@ namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day4 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        double sum = inputRepository.FetchLines(inputRef)
+        Dictionary<int, ScratchCard> scratchCards = inputRepository.FetchLines(inputRef)
                 .Select(line => line.Replace("Card ", ""))
                 .Select(line => Regex.Replace(line, @"\s+", " "))
                 .Select(line => {
@@ -17,16 +17,22 @@ public class Day4 : IDailyProgram {
                     ISet<int> received = winningAndReceived[1].Split(" ")
                             .Select(int.Parse)
                             .ToHashSet();
-                    return new {
-                            CardNumber = int.Parse(gameIdAndRest[0]),
-                            Winning = winning,
-                            Received = received,
-                    };
+                    return new ScratchCard(
+                            int.Parse(gameIdAndRest[0]),
+                            winning.Intersect(received).Count()
+                    );
                 })
-                .Select(obj => obj.Winning.Intersect(obj.Received).Count())
-                .Where(winningCount => winningCount > 0)
-                .Select(winningCount => Math.Pow(2, winningCount - 1))
-                .Sum();
-        Console.WriteLine($"Total winning points: {sum}");
+                .ToDictionary(card => card.CardNumber);
+
+        if (part == 1) {
+            double sum = scratchCards.Values
+                    .Select(card => card.Value)
+                    .Sum();
+            Console.WriteLine($"Total winning points: {sum}");
+        }
     }
 }
+
+public readonly record struct ScratchCard(int CardNumber, int WinningCount) {
+    public int Value => WinningCount == 0 ? 0 : (int)Math.Pow(2, WinningCount - 1);
+};
