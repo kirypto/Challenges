8ea2212ff54eae6f314a529e80d340969de3da49
kirypto <kirypto@gmail.com>
2023-12-05 16:53:45 -0600

    AOC-2023 Finish day 5 part 1

--- AdventOfCode/2023/DailyPrograms/Day5.cs
+++ AdventOfCode/2023/DailyPrograms/Day5.cs
@@ -11,11 +11,9 @@ public class Day5 : IDailyProgram {
                 .FetchLines(inputRef)
                 .Where(s => s.Any())
                 .ToList();
-        ISet<int> seeds = Regex.Matches(inputLines[0], @"(\d+)")
-                .Select(match => int.Parse(match.Value))
+        ISet<long> seeds = Regex.Matches(inputLines[0], @"(\d+)")
+                .Select(match => long.Parse(match.Value))
                 .ToHashSet();
-        Console.WriteLine("Seeds:");
-        seeds.ForEach(Console.WriteLine);
         IDictionary<string, IList<AlmanacMapper>> mappers = new Dictionary<string, IList<AlmanacMapper>>();
         var currentMapper = "";
         foreach (string line in inputLines.Skip(1)) {
@@ -26,33 +24,50 @@ public class Day5 : IDailyProgram {
                 mappers[currentMapper] = new List<AlmanacMapper>();
             } else {
                 mappers[currentMapper].Add(AlmanacMapperFrom(
-                        matches.Select(match => int.Parse(match.Value)).ToList()
+                        matches.Select(match => long.Parse(match.Value)).ToList()
                 ));
             }
         }
         var almanac = new Almamac(mappers);
-        throw new NotImplementedException();
+        long minLocation = seeds.Select(seed => almanac.MapSeedToLocation(seed))
+                .Min();
+        Console.WriteLine($"Min location: {minLocation}");
     }
 }
 
-public readonly record struct Almamac(IDictionary<string, IList<AlmanacMapper>> Mappers) {
+public record Almamac(IDictionary<string, IList<AlmanacMapper>> Mappers) {
+    public long MapSeedToLocation(long seed) => new List<string> {
+                    "seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light",
+                    "light-to-temperature", "temperature-to-humidity", "humidity-to-location",
+            }
+            .Select(mapName => Mappers[mapName])
+            .Aggregate(seed, (current, categoryMappers) => categoryMappers.Map(current));
 }
 
-public readonly record struct AlmanacMapper(MapperFunc Mapper, IsApplicableFunction IsApplicable) {
-    public delegate int MapperFunc(int input);
+public readonly record struct AlmanacMapper(MapperFunc Map, IsApplicableFunction IsApplicable) {
+    public delegate long MapperFunc(long input);
 
-    public delegate bool IsApplicableFunction(int input);
+    public delegate bool IsApplicableFunction(long input);
 
     public static AlmanacMapper Identity() {
-        return new AlmanacMapper(input => input, input => true);
+        return new AlmanacMapper(input => input, _ => true);
     }
 
-    public static AlmanacMapper AlmanacMapperFrom(IList<int> args) {
-        int destinationRangeStart = args[0];
-        int sourceRangeStart = args[1];
-        int rangeLength = args[2];
+    public static AlmanacMapper AlmanacMapperFrom(IList<long> args) {
+        long destinationRangeStart = args[0];
+        long sourceRangeStart = args[1];
+        long rangeLength = args[2];
         return new AlmanacMapper(
                 input => input + (destinationRangeStart - sourceRangeStart),
                 input => sourceRangeStart <= input && input < sourceRangeStart + rangeLength);
+    }
+}
+
+public static class AlmanacMapperExtensions {
+    public static long Map(this IList<AlmanacMapper> mappers, long input) {
+        return mappers
+                .Where(mapper => mapper.IsApplicable(input))
+                .FirstOrDefault(Identity())
+                .Map(input);
     }
 }
