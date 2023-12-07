984e1225bb72137b1e1f4f1e3da4c2d0adbe937e
kirypto <kirypto@gmail.com>
2023-12-06 21:47:53 -0600

    AOC-2023 Move day 5 helper structs back to main file

--- AdventOfCode/2023/DailyPrograms/Day5.cs
+++ AdventOfCode/2023/DailyPrograms/Day5.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using C5;
 using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
 
@@ -9,7 +10,7 @@ namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day5 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        IList<string> inputLines = inputRepository
+        System.Collections.Generic.IList<string> inputLines = inputRepository
                 .FetchLines(inputRef)
                 .Where(s => s.Any())
                 .ToList();
@@ -27,11 +28,9 @@ public class Day5 : IDailyProgram {
         }
 
 
-
         Console.WriteLine(seeds.Count);
         var currentEntryList = new List<AlmanacMapEntry>();
-        IList<AlmanacMap> almanacMaps = new List<AlmanacMap>();
-        var currentMap = "";
+        System.Collections.Generic.IList<AlmanacMap> almanacMaps = new List<AlmanacMap>();
         foreach (string line in inputLines.Skip(1)) {
             var mapNameMatch = Regex.Match(line, @"^([\w-]+)(?=( map:$))");
             MatchCollection matches = Regex.Matches(line, @"(\d+)");
@@ -52,11 +51,45 @@ public class Day5 : IDailyProgram {
         throw new NotImplementedException();
     }
 
-    private static IEnumerable<long> LongRange(long start, long count)
-    {
-        for (long i = 0; i < count; i++)
-        {
+    private static IEnumerable<long> LongRange(long start, long count) {
+        for (long i = 0; i < count; i++) {
             yield return start + i;
+        }
+    }
+}
+
+public readonly record struct AlmanacMapEntry(long destinationRangeStart, long sourceRangeStart, long rangeLength);
+
+public readonly record struct AlmanacMap {
+    private readonly TreeDictionary<long, long> _ranges;
+    private string Name { get; }
+
+    public AlmanacMap(string name, System.Collections.Generic.ICollection<AlmanacMapEntry> entries) {
+        Name = name;
+        var ranges = new TreeDictionary<long, long> {
+                [0] = 0,
+        };
+        foreach ((long destinationRangeStart, long sourceRangeStart, long rangeLength) in entries
+                         .OrderBy(e => e.sourceRangeStart)) {
+            C5.KeyValuePair<long,long> newAfter = ranges.WeakPredecessor(sourceRangeStart + rangeLength);
+            C5.KeyValuePair<long,long> existingBefore = ranges.WeakPredecessor(sourceRangeStart);
+            if (newAfter != existingBefore) {
+                throw new NotImplementedException("Clean slice insert doesn't work...");
+            }
+            ranges[sourceRangeStart + rangeLength] = newAfter.Value;
+            ranges[sourceRangeStart] = destinationRangeStart - sourceRangeStart;
+        }
+        _ranges = ranges;
+    }
+
+    public long Map(long input) {
+        return input + _ranges.WeakPredecessor(input).Value;
+    }
+
+    public void PrintToConsole() {
+        Console.WriteLine($"Almanac Map '{Name}'");
+        foreach (C5.KeyValuePair<long, long> keyValuePair in _ranges) {
+            Console.WriteLine($"{keyValuePair.Key} -> {keyValuePair.Value}");
         }
     }
 }
