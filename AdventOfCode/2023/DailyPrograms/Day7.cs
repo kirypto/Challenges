e21dfae092148b001ca62a3f3aece5376784c3e6
kirypto <kirypto@gmail.com>
2023-12-07 12:19:09 -0600

    AOC-2023 Setup day 7 answer, without card rank ordering

--- AdventOfCode/2023/DailyPrograms/Day7.cs
+++ AdventOfCode/2023/DailyPrograms/Day7.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
@@ -8,13 +7,16 @@ namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day7 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        List<Hand> hands = inputRepository.FetchLines(inputRef)
+        long totalWinnings = inputRepository.FetchLines(inputRef)
                 .Select(inputLine => inputLine.Split(" "))
                 .Select(cardsAndBid => new Hand(cardsAndBid[0], long.Parse(cardsAndBid[1])))
-                .OrderDescending()
-                .ToList();
-        hands.Select(hand => hand.ToString()).ForEach(Console.WriteLine);
+                .Order()
+                .Tap(hand => Console.Write($"--> {hand}: "))
+                .Select((hand, valueRank) => hand.Bid * valueRank)
+                .Tap(Console.WriteLine)
+                .Aggregate((w1, w2) => w1 + w2);
 
+        Console.WriteLine($"Total Winnings: {totalWinnings}");
         throw new NotImplementedException();
     }
 }
@@ -22,11 +24,11 @@ public class Day7 : IDailyProgram {
 public readonly record struct Hand(string Cards, long Bid, long Rank) : IComparable<Hand> {
     public Hand(string Cards, long Bid) : this(Cards, Bid, DeriveRank(Cards, Bid)) { }
 
-    private static long DeriveRank(string cards, long bid) {
-        return -1;
-    }
-
     public int CompareTo(Hand other) {
         return Rank.CompareTo(other.Rank);
+    }
+
+    private static long DeriveRank(string cards, long bid) {
+        return -1;
     }
 }
--- AdventOfCode/2023/Extensions/EnumerableExtensions.cs
+++ AdventOfCode/2023/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace kirypto.AdventOfCode._2023.Extensions;
 
@@ -8,5 +9,12 @@ public static class EnumerableExtensions {
         foreach (T t in enumerable) {
             action(t);
         }
+    }
+
+    public static IEnumerable<T> Tap<T>(this IEnumerable<T> enumerable, Action<T> action) {
+        return enumerable.Select(t => {
+            action(t);
+            return t;
+        });
     }
 }
