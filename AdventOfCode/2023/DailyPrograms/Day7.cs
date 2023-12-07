25cc148fb8b421ded32d50b025f8e5720cc6882d
kirypto <kirypto@gmail.com>
2023-12-07 12:07:34 -0600

    AOC-2023 Process day 7 input

--- AdventOfCode/2023/DailyPrograms/Day7.cs
+++ AdventOfCode/2023/DailyPrograms/Day7.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using kirypto.AdventOfCode._2023.Extensions;
 using kirypto.AdventOfCode._2023.Repos;
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day7 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
+        List<Hand> hands = inputRepository.FetchLines(inputRef)
+                .Select(inputLine => inputLine.Split(" "))
+                .Select(cardsAndBid => new Hand(cardsAndBid[0], long.Parse(cardsAndBid[1])))
+                .OrderDescending()
+                .ToList();
+        hands.Select(hand => hand.ToString()).ForEach(Console.WriteLine);
+
         throw new NotImplementedException();
+    }
+}
+
+public readonly record struct Hand(string Cards, long Bid, long Rank) : IComparable<Hand> {
+    public Hand(string Cards, long Bid) : this(Cards, Bid, DeriveRank(Cards, Bid)) { }
+
+    private static long DeriveRank(string cards, long bid) {
+        return -1;
+    }
+
+    public int CompareTo(Hand other) {
+        return Rank.CompareTo(other.Rank);
     }
 }
