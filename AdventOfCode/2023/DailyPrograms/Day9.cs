1cad9c00d089e212c4c12f4e09ac892b1914cc67
kirypto <kirypto@gmail.com>
2023-12-10 16:44:19 -0600

    AOC-2023 Parsing day 9 input

--- AdventOfCode/2023/DailyPrograms/Day9.cs
+++ AdventOfCode/2023/DailyPrograms/Day9.cs
@@ -1,9 +1,30 @@
-﻿using kirypto.AdventOfCode._2023.Repos;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using kirypto.AdventOfCode._2023.Repos;
+using static kirypto.AdventOfCode._2023.DailyPrograms.OasisHistoryRecord;
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day9 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        throw new System.NotImplementedException();
+        List<OasisHistoryRecord> historyRecords = inputRepository.FetchLines(inputRef)
+                .Select(ParseOasisHistoryRecord)
+                .ToList();
+
+        historyRecords.ForEach(history => Console.WriteLine(string.Join(",", history.Observations)));
     }
+}
+
+public readonly partial record struct OasisHistoryRecord(IList<long> Observations) {
+    public static OasisHistoryRecord ParseOasisHistoryRecord(string recordString) {
+        return new OasisHistoryRecord(
+                OasisHistoryRecordEntryPattern().Matches(recordString)
+                        .Select(match => long.Parse(match.Value))
+                        .ToList());
+    }
+
+    [GeneratedRegex("(\\d+)")]
+    private static partial Regex OasisHistoryRecordEntryPattern();
 }
