687eb341431f2906dc677c28056cf7d04684b615
kirypto <kirypto@gmail.com>
2023-12-14 12:57:01 -0600

    AOC-2023 Cleanup day 14 prep for part 2

--- AdventOfCode/2023/DailyPrograms/Day14.cs
+++ AdventOfCode/2023/DailyPrograms/Day14.cs
@@ -7,29 +7,55 @@ namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day14 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        char[,] map = inputRepository.Fetch(inputRef)
+        char[,] rockPlatform = inputRepository.Fetch(inputRef)
                 .Trim()
                 .To2DCharArray("\n");
-        int rowCount = map.GetLength(0);
-        int colCount = map.GetLength(1);
-        int totalLoad = Range(0, colCount)
-                .Select(col => {
-                    int lastRockIndex = -1;
-                    // Console.Write($"[Col {col}] ");
-                    int sum = map.EnumerateCol(col)
-                            .Select((c, i) => c switch {
-                                    'O' => rowCount - ++lastRockIndex,
-                                    '#' => (lastRockIndex = i) - lastRockIndex,
-                                    '.' => 0,
-                                    _ => throw new NotImplementedException($"Unhandled char: {c}"),
-                            })
-                            // .Tap(load => Console.Write($"{load} "))
-                            .Sum();
-                    // Console.WriteLine($"--> {sum}");
-                    return sum;
-                })
-                .Sum();
 
-        Console.WriteLine($"Total load: {totalLoad}");
+        if (part == 1) {
+            rockPlatform.TiltNorth();
+            // rockPlatform.PrintToConsole();
+            int totalLoad = rockPlatform.DetermineNorthernLoad();
+            Console.WriteLine($"Total load: {totalLoad}");
+        } else {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+public static class RockPlatformExtensions {
+    public static void TiltNorth(this char[,] rockPlatform) {
+        int rowCount = rockPlatform.GetLength(0);
+        int colCount = rockPlatform.GetLength(1);
+        for (var col = 0; col < colCount; col++) {
+            int lastRockIndex = -1;
+            for (var row = 0; row < rowCount; row++) {
+                char c = rockPlatform[row, col];
+                switch (c) {
+                    case 'O':
+                        lastRockIndex++;
+                        rockPlatform[row, col] = '.';
+                        rockPlatform[lastRockIndex, col] = 'O';
+                        break;
+                    case '#':
+                        lastRockIndex = row;
+                        break;
+                }
+            }
+        }
+    }
+
+    public static int DetermineNorthernLoad(this char[,] rockPlatform) {
+        int rowCount = rockPlatform.GetLength(0);
+        int colCount = rockPlatform.GetLength(1);
+        return Range(0, colCount)
+                .Select(col => rockPlatform.EnumerateCol(col)
+                        .Select((c, i) => c switch {
+                                'O' => rowCount - i,
+                                '#' => 0,
+                                '.' => 0,
+                                _ => throw new NotImplementedException($"Unhandled char: {c}"),
+                        })
+                        .Sum())
+                .Sum();
     }
 }
