3609385e7caaa0b9626e482b8fd9a3d13f9fe4c7
kirypto <kirypto@gmail.com>
2023-12-16 12:43:45 -0600

    AOC-2023 Implement a generic ReflectsAt extension function for IEnumerable<T>

--- AdventOfCode/2023/DailyPrograms/Day13.cs
+++ AdventOfCode/2023/DailyPrograms/Day13.cs
@@ -1,9 +1,28 @@
-﻿using kirypto.AdventOfCode._2023.Repos;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using kirypto.AdventOfCode._2023.Extensions;
+using kirypto.AdventOfCode._2023.Repos;
+using MirrorMap = char[,];
 
 namespace kirypto.AdventOfCode._2023.DailyPrograms;
 
 public class Day13 : IDailyProgram {
     public void Run(IInputRepository inputRepository, string inputRef, int part) {
-        throw new System.NotImplementedException();
+        const string test = "234554321";
+        Console.WriteLine($"Checking reflections of {test}: ");
+        Enumerable.Range(0, test.Length)
+                .Tap(i => Console.Write($"  @ {i}: "))
+                .Select(i => test.ReflectsAt(i))
+                .ForEach(Console.WriteLine);
+
+        throw new NotImplementedException();
+        int foo = inputRepository.Fetch(inputRef)
+                .Split("\n\n")
+                .Select(mirrorMap => mirrorMap.To2DCharArray("\n"))
+                .Tap(mirrorMap => mirrorMap.PrintToConsole())
+                .Count();
+        Console.WriteLine(foo);
     }
 }
+
--- AdventOfCode/2023/Extensions/EnumerableExtensions.cs
+++ AdventOfCode/2023/Extensions/EnumerableExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace kirypto.AdventOfCode._2023.Extensions;
 
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 public static class EnumerableExtensions {
     public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action) {
         foreach (T t in enumerable) {
@@ -30,5 +32,42 @@ public static class EnumerableExtensions {
             }
         }
         // ReSharper disable once IteratorNeverReturns
+    }
+
+    public static bool ReflectsAt<T>(this IEnumerable<T> enumerable, int index) {
+        using IEnumerator<T> mirrorEnumerator = enumerable.GetEnumerator();
+        var observed = new List<T>();
+        int i = -1;
+        while (++i < index && mirrorEnumerator.GetNext(out T current)) {
+            // Console.WriteLine($"Index {i}: {current}");
+            observed.Add(current);
+        }
+        for (int j = observed.Count - 1; j >= 0; j--) {
+            if (mirrorEnumerator.GetNext(out T current)) {
+                if (current != null && !current.Equals(observed[j])) {
+                    return false;
+                }
+            } else {
+                break;
+            }
+        }
+        return observed.Any();
+    }
+
+    public static bool GetNext<T>(this IEnumerator<T> enumerator, out T t) {
+        try {
+            t = enumerator.Current;
+        }
+        catch (InvalidOperationException) {
+            // Hasn't yet started enumerating;
+            bool moveSuccessful = enumerator.MoveNext();
+            t = enumerator.Current;
+            return moveSuccessful;
+        }
+        if (enumerator.MoveNext()) {
+            t = enumerator.Current;
+            return true;
+        }
+        return false;
     }
 }
