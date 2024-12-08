548489300db2eb93db43f68925e13c938f799971
kirypto <kirypto@gmail.com>
2024-12-08 14:25:44 -0600

    AOC-2024 Remove subfolder logic from FileInputRepository

--- AdventOfCode/Common.CS/Repositories/FileInputRepository.cs
+++ AdventOfCode/Common.CS/Repositories/FileInputRepository.cs
@@ -4,10 +4,7 @@ using kirypto.AdventOfCode.Common.Interfaces;
 namespace kirypto.AdventOfCode.Common.Repositories;
 
 public class FileInputRepository : IInputRepository {
-    private const string _INPUTS_DIR = "Data\\InputFiles";
-
     public string Fetch(string inputRef) {
-        string inputFilePath = Path.Combine(_INPUTS_DIR, inputRef.EndsWith(".txt") ? inputRef : inputRef + ".txt");
-        return File.ReadAllText(inputFilePath);
+        return File.ReadAllText(inputRef);
     }
 }
