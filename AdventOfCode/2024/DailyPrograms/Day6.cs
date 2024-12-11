c39fff71ed22354c3fb1abe8bfe3fe6bbba5f8b3
kirypto <kirypto@gmail.com>
2024-12-10 20:26:52 -0600

    AOC-2024 Solve day 6 part 2

--- AdventOfCode/2024/DailyPrograms/Day6.cs
+++ AdventOfCode/2024/DailyPrograms/Day6.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using kirypto.AdventOfCode.Common.Attributes;
 using kirypto.AdventOfCode.Common.Interfaces;
 using Microsoft.Extensions.Logging;
+using static kirypto.AdventOfCode._2024.DailyPrograms.CardinalDirection;
 using static kirypto.AdventOfCode.Common.Services.IO.DailyProgramLogger;
 
 namespace kirypto.AdventOfCode._2024.DailyPrograms;
@@ -10,13 +13,12 @@ namespace kirypto.AdventOfCode._2024.DailyPrograms;
 public class Day6 : IDailyProgram {
     public string Run(IInputRepository inputRepository, string inputRef, int part) {
         Logger.LogInformation("Day6");
-        var mapLines = inputRepository.FetchLines(inputRef);
+        IList<string> mapLines = inputRepository.FetchLines(inputRef);
         int rowCount = mapLines.Count;
         int colCount = mapLines[0].Length;
 
-        var map = new Cell[rowCount, colCount];
-        Coord position = new Coord(0, 0);
-        CardinalDirection direction = CardinalDirection.North;
+        Cell[,] map = new Cell[rowCount, colCount];
+        DirectedCoord position = new(new Coord(0, 0), North);
         for (int row = 0; row < rowCount; row++) {
             for (int col = 0; col < colCount; col++) {
                 char cellChar = mapLines[row][col];
@@ -27,65 +29,93 @@ public class Day6 : IDailyProgram {
                         _ => throw new ArgumentException($"Invalid cell char '{cellChar}'"),
                 };
                 if (cellChar == '^') {
-                    position = new Coord(col, row);
+                    position = position with { Coord = new Coord(col, row) };
                 }
             }
         }
         Logger.LogInformation($"Initial position: {position}");
 
-        int distinctVisitCellCount = 0;
-        int possibleLoopCounts = 0;
-        while (IsWithinBounds(position, rowCount, colCount)) {
-            Logger.LogInformation($"Current: {position} {direction}");
-
-            if (map[position.Y, position.X].WasVisited()) {
-                distinctVisitCellCount++;
-            }
-            map[position.Y, position.X].SetVisited(direction);
-
-            Coord nextPosition = position.Move(direction);
-            if (!IsWithinBounds(nextPosition, rowCount, colCount)) {
-                Logger.LogInformation("Found edge");
-                break;
-            }
-
-            if (CouldMakeLoop(position, nextPosition, direction, map)) {
-                Logger.LogInformation("Found loop");
-                possibleLoopCounts++;
-            }
-            Cell nextCell = map[nextPosition.Y, nextPosition.X];
-            if (nextCell.Obstacle) {
-                Logger.LogInformation("Found obstacle");
-                direction = direction.Rotate90Clockwise();
-                continue;
-            }
-            position = nextPosition;
+        (ExitType _, ISet<DirectedCoord> visited) = SimulateGuard(map, position);
+        if (part == 1) {
+            return visited.Select(pos => pos.Coord).Distinct().Count().ToString();
         }
-        Logger.LogInformation($"Part 1: {distinctVisitCellCount}, part 2: {possibleLoopCounts}");
-        return (part == 1 ? distinctVisitCellCount : possibleLoopCounts).ToString();
+        HashSet<Coord> loopingObstacles = [];
+        int counter = 0;
+        foreach (Coord obstaclePosition in visited
+                         .Select(pos => pos.Coord)
+                         .Distinct()
+                         .Except([position.Coord])) {
+            Cell[,] modifiedMap = CloneWithObstacleAt(map, obstaclePosition);
+            counter++;
+            Logger.LogInformation($"Checking loop position {counter} of {visited.Count}");
+            (ExitType exit, ISet<DirectedCoord> _) = SimulateGuard(modifiedMap, position);
+            if (exit is ExitType.Loop) {
+                loopingObstacles.Add(obstaclePosition);
+            }
+        }
+        return loopingObstacles.Count.ToString();
     }
 
-    private static bool CouldMakeLoop(Coord position, Coord nextPosition, CardinalDirection direction, Cell[,] map) {
-        bool nextIsObstacle = map[nextPosition.Y, nextPosition.X].Obstacle;
-        CardinalDirection directionIfTurned = direction.Rotate90Clockwise();
-        bool wouldBeLoopIfTurnedInPlace = map[position.Y, position.X].WasVisited(directionIfTurned);
-        return wouldBeLoopIfTurnedInPlace && !nextIsObstacle;
+    private static (ExitType exit, ISet<DirectedCoord> visited) SimulateGuard(Cell[,] map, DirectedCoord position) {
+        int rowCount = map.GetLength(0);
+        int colCount = map.GetLength(1);
+        HashSet<DirectedCoord> visited = [];
+        while (IsWithinBounds(position.Coord, rowCount, colCount)) {
+            if (!visited.Add(position)) {
+                return (ExitType.Loop, visited);
+            }
+
+            DirectedCoord next = position.MoveForward();
+            if (IsWithinBounds(next.Coord, rowCount, colCount) && map[next.Y, next.X].Obstacle) {
+                // Rotate
+                position = position.Rotate90Clockwise();
+            } else {
+                // Move Forward
+                position = next;
+            }
+
+        }
+        return (ExitType.Escape, visited);
+    }
+
+    private static Cell[,] CloneWithObstacleAt(Cell[,] map, Coord coord) {
+        Cell[,] modifiedMap = (Cell[,])map.Clone();
+        modifiedMap[coord.Y, coord.X] = new Cell { Obstacle = true };
+        return modifiedMap;
     }
 
     private static bool IsWithinBounds(Coord position, int rowCount, int colCount) {
-        return position.X >= 0 && position.Y >= 0 && position.X < colCount && position.Y < rowCount;
+        return position is { X: >= 0, Y: >= 0 } && position.X < colCount && position.Y < rowCount;
+    }
+
+    private enum ExitType {
+        Escape,
+        Loop,
     }
 }
 
 public readonly record struct Coord(int X, int Y);
 
+public readonly record struct DirectedCoord(Coord Coord, CardinalDirection Direction) {
+    public int Y => Coord.Y;
+    public int X => Coord.X;
+
+    public DirectedCoord MoveForward() {
+        return this with { Coord = Coord.Move(Direction) };
+    }
+
+    public DirectedCoord Rotate90Clockwise() {
+        return this with { Direction = Direction.Rotate90Clockwise() };
+    }
+}
+
 public static class CoordExtensions {
     public static Coord Move(this Coord coord, CardinalDirection direction) {
         return direction switch {
-                CardinalDirection.North => coord with { Y = coord.Y - 1 },
-                CardinalDirection.East => coord with { X = coord.X + 1 },
-                CardinalDirection.South => coord with { Y = coord.Y + 1 },
-                CardinalDirection.West => coord with { X = coord.X - 1 },
+                North => coord with { Y = coord.Y - 1 },
+                East => coord with { X = coord.X + 1 },
+                South => coord with { Y = coord.Y + 1 },
+                West => coord with { X = coord.X - 1 },
                 _ => throw new ArgumentException($"Unsupported direction {direction}", nameof(direction)),
         };
     }
@@ -101,45 +131,13 @@ public enum CardinalDirection {
 public static class CardinalDirectionExtensions {
     public static CardinalDirection Rotate90Clockwise(this CardinalDirection direction) {
         return direction switch {
-                CardinalDirection.North => CardinalDirection.East,
-                CardinalDirection.East => CardinalDirection.South,
-                CardinalDirection.South => CardinalDirection.West,
-                CardinalDirection.West => CardinalDirection.North,
+                North => East,
+                East => South,
+                South => West,
+                West => North,
                 _ => throw new ArgumentException($"Unsupported direction {direction}", nameof(direction)),
         };
     }
 }
 
-internal record struct Cell(
-        bool Obstacle,
-        bool VisitedFacingNorth,
-        bool VisitedFacingEast,
-        bool VisitedFacingSouth,
-        bool VisitedFacingWest) {
-    internal bool WasVisited(CardinalDirection? facing = null) {
-        return facing switch {
-                CardinalDirection.North => VisitedFacingNorth,
-                CardinalDirection.East => VisitedFacingEast,
-                CardinalDirection.South => VisitedFacingSouth,
-                CardinalDirection.West => VisitedFacingWest,
-                _ => VisitedFacingNorth || VisitedFacingEast || VisitedFacingSouth || VisitedFacingSouth,
-        };
-    }
-
-    internal void SetVisited(CardinalDirection facing) {
-        switch (facing) {
-            case CardinalDirection.North:
-                VisitedFacingNorth = true;
-                break;
-            case CardinalDirection.East:
-                VisitedFacingEast = true;
-                break;
-            case CardinalDirection.South:
-                VisitedFacingSouth = true;
-                break;
-            case CardinalDirection.West:
-                VisitedFacingWest = true;
-                break;
-        }
-    }
-}
+internal record struct Cell(bool Obstacle);
