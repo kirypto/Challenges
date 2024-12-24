55e9cb66cc0a86e467827c6fc37432519bdabeb5
kirypto <kirypto@gmail.com>
2024-12-24 16:35:26 -0600

    AOC-2024 [speed run] Working on day 2, est 90%

---
+++ AdventOfCode/2024/DailyPrograms/Day02.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using kirypto.AdventOfCode.Common.Attributes;
+using kirypto.AdventOfCode.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using static kirypto.AdventOfCode.Common.Services.IO.DailyProgramLogger;
+
+namespace kirypto.AdventOfCode._2024.DailyPrograms;
+
+// ReSharper disable once UnusedType.Global
+[DailyProgram(2)]
+public class Day02 : IDailyProgram {
+    public string Run(IInputRepository inputRepository, int part) {
+        List<List<int>> lines = inputRepository.FetchLines()
+                .Select(line => line.Split(" ")
+                        .Select(numericChar => int.Parse(numericChar.ToString()))
+                        .ToList())
+                .ToList();
+
+        foreach (List<int> line in lines) {
+            bool increasing = line[0] < line[1];
+            Logger.LogInformation(increasing ? "increasing" : "decreasing");
+            bool isSafe = true;
+            for (int i = 0; i < line.Count - 1; i++) {
+                int curr = line[i];
+                int next = line[i + 1];
+                int diff = next - curr;
+                if (increasing && diff is < 1 or > 2) {
+                    isSafe = false;
+                    break;
+                }
+                if (!increasing && diff is > -1 or < -2) {
+                    isSafe = false;
+                    break;
+                }
+            }
+            Logger.LogInformation("Is safe: {isSafe}", isSafe);
+        }
+
+
+        throw new System.NotImplementedException();
+    }
+}
