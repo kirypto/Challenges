b7ec537d0e1f9dc37cbff821d5a1667ee1285933
kirypto <kirypto@gmail.com>
2024-12-24 16:46:21 -0600

    AOC-2024 [speed run] Done day 3 part 1!

---
+++ AdventOfCode/2024/DailyPrograms/Day03.cs
@@ -0,0 +1,29 @@
+using System.Text.RegularExpressions;
+using kirypto.AdventOfCode.Common.Attributes;
+using kirypto.AdventOfCode.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using static kirypto.AdventOfCode.Common.Services.IO.DailyProgramLogger;
+
+namespace kirypto.AdventOfCode._2024.DailyPrograms;
+
+// ReSharper disable once UnusedType.Global
+[DailyProgram(3)]
+public partial class Day03 : IDailyProgram {
+    public string Run(IInputRepository inputRepository, int part) {
+        MatchCollection matchCollection = MulInstructionPattern().Matches(inputRepository.Fetch());
+        Logger.LogInformation("Found {count} instructions", matchCollection.Count);
+        int total = 0;
+        foreach (Match match in matchCollection) {
+            Logger.LogInformation(match.ToString());
+            int valueA = int.Parse(match.Groups[1].Value);
+            int valueB = int.Parse(match.Groups[2].Value);
+            int result = valueA * valueB;
+            Logger.LogInformation("{a} * {b} = {result}", valueA, valueB, result);
+            total += result;
+        }
+        return total.ToString();
+    }
+
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex MulInstructionPattern();
+}
