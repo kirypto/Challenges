c59215a141d58617900aa3c0b19e75507c49aabc
kirypto <kirypto@gmail.com>
2024-12-24 17:20:45 -0600

    AOC-2024 [speed run] Working on day 7, est 40%

---
+++ AdventOfCode/2024/DailyPrograms/Day07.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using kirypto.AdventOfCode.Common.Attributes;
+using kirypto.AdventOfCode.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using static System.StringSplitOptions;
+using static kirypto.AdventOfCode.Common.Services.IO.DailyProgramLogger;
+
+namespace kirypto.AdventOfCode._2024.DailyPrograms;
+
+// ReSharper disable once UnusedType.Global
+[DailyProgram(7)]
+public class Day07 : IDailyProgram {
+    public string Run(IInputRepository inputRepository, int part) {
+        List<Equation> equations = inputRepository.FetchLines()
+                .Select(Equation.Parse)
+                .ToList();
+        Logger.LogInformation("Found {count} equations", equations.Count);
+
+        HashSet<Func<int, int, int>> operators = [
+            (a, b) => a + b,
+            (a, b) => a * b,
+        ];
+
+        foreach (Equation equation in equations) {
+            // Walk through
+        }
+
+        throw new System.NotImplementedException();
+    }
+
+
+
+    private readonly record struct Equation(int Result, IList<int> Values) {
+        public static Func<string, Equation> Parse => raw => {
+            string[] resultAndValues = raw.Split(":");
+            return new Equation {
+                    Result = int.Parse(resultAndValues[0]),
+                    Values = resultAndValues[1].Split(" ", TrimEntries | RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToList(),
+            };
+        };
+    }
+}
