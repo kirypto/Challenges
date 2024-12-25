a01d2f681f2e283f51ddc161abc758fe7e82d935
kirypto <kirypto@gmail.com>
2024-12-25 11:10:54 -0600

    AOC-2024 [speed run] Working on day 17, est like 5%...

---
+++ AdventOfCode/2024/DailyPrograms/Day17.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using kirypto.AdventOfCode.Common.Attributes;
+using kirypto.AdventOfCode.Common.Interfaces;
+
+namespace kirypto.AdventOfCode._2024.DailyPrograms;
+
+// ReSharper disable once UnusedType.Global
+[DailyProgram(17)]
+public class Day17: IDailyProgram {
+    public string Run(IInputRepository inputRepository, int part) {
+        string[] registersAndProgram = inputRepository.Fetch()
+                .Replace(" ", "")
+                .Replace("Register", "")
+                .Replace("Program ", "")
+                .Split("\n\n");
+        Dictionary<string,int> registers = registersAndProgram[0]
+                .Split("\n")
+                .Select(line => line.Split(":"))
+                .ToDictionary(pair => pair[0], nameAndValue => int.Parse(nameAndValue[1]));
+
+        throw new NotImplementedException();
+    }
+}
