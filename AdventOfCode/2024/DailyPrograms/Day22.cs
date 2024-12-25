468e2d3e22b25d0166107a4fa134e9d4624a5779
kirypto <kirypto@gmail.com>
2024-12-25 11:51:30 -0600

    AOC-2024 [speed run] Working on day 22, est 15%

---
+++ AdventOfCode/2024/DailyPrograms/Day22.cs
@@ -0,0 +1,19 @@
+using kirypto.AdventOfCode.Common.Attributes;
+using kirypto.AdventOfCode.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace kirypto.AdventOfCode._2024.DailyPrograms;
+
+// ReSharper disable once UnusedType.Global
+[DailyProgram(22)]
+public class Day22 : IDailyProgram {
+    public string Run(IInputRepository inputRepository, int part) {
+        inputRepository.Fetch();
+        Logger.LogInformation("Mix 15 into 42 (expecting 37): {result}", Mix(42, 15));
+        Logger.LogInformation("Prune 100000000 (expecting 16113920): {result}", Prune(100000000));
+        throw new System.NotImplementedException();
+    }
+
+    private static long Mix(long secretNumber, long newNumber) => secretNumber ^ newNumber;
+    private static long Prune(long secretNumber) => secretNumber % 16777216L;
+}
