57dcd0ec89cab8fe07b85f294ef88dd62b421be0
kirypto <kirypto@gmail.com>
2024-12-26 14:44:07 -0600

    AOC-2024 [speed run] Working on day 22, est 25%

--- AdventOfCode/2024/DailyPrograms/Day22.cs
+++ AdventOfCode/2024/DailyPrograms/Day22.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using kirypto.AdventOfCode.Common.AOC;
 using kirypto.AdventOfCode.Common.Repositories;
 using Microsoft.Extensions.Logging;
@@ -10,10 +11,30 @@ namespace kirypto.AdventOfCode._2024.DailyPrograms;
 public class Day22 : IDailyProgram {
     public string Run(IInputRepository inputRepository, int part) {
         inputRepository.Fetch();
-        Logger.LogInformation("Mix 15 into 42 (expecting 37): {result}", Mix(42, 15));
-        Logger.LogInformation("Prune 100000000 (expecting 16113920): {result}", Prune(100000000));
+        RunTests();
         throw new NotImplementedException();
     }
+
+    private static void RunTests() {
+        if (!Program.IsVerbose) {
+            return;
+        }
+        Logger.LogInformation("Mix 15 into 42 (expecting 37): {result}", Mix(42, 15));
+        Logger.LogInformation("Prune 100000000 (expecting 16113920): {result}", Prune(100000000));
+        long secretNumber = 123;
+        Logger.LogInformation("Sample, starting at 123:");
+        for (int iteration = 0; iteration < 10; iteration++) {
+            secretNumber = IterateSecretNumber(iteration, secretNumber);
+            Logger.LogInformation("At {iteration} number is {secretNum}", iteration, secretNumber);
+        }
+    }
+
+    private static long IterateSecretNumber(int iteration, long secretNumber) => (iteration % 3) switch {
+            0 => Prune(Mix(secretNumber, 64 * secretNumber)),
+            1 => Prune(Mix(secretNumber, secretNumber / 32)),
+            2 => Prune(Mix(secretNumber, secretNumber * 2048)),
+            _ => throw new InvalidOperationException($"Somehow it didn't work... iteration was {iteration}"),
+    };
 
     private static long Mix(long secretNumber, long newNumber) => secretNumber ^ newNumber;
     private static long Prune(long secretNumber) => secretNumber % 16777216L;
