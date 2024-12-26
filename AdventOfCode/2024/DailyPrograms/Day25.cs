444a24c98f52c707f3291413289b614091019c96
kirypto <kirypto@gmail.com>
2024-12-25 23:58:52 -0600

    AOC-2024 Solve day 25 part 1

--- AdventOfCode/2024/DailyPrograms/Day25.cs
+++ AdventOfCode/2024/DailyPrograms/Day25.cs
@@ -12,30 +12,52 @@ namespace kirypto.AdventOfCode._2024.DailyPrograms;
 [DailyProgram(25)]
 public class Day25 : IDailyProgram {
     public string Run(IInputRepository inputRepository, int part) {
-        List<long> keysAndLocks = inputRepository.Fetch()
+        List<long> locks = [];
+        List<long> keys = [];
+                inputRepository.Fetch()
                 .Replace("#", "1")
                 .Replace(".", "0")
                 .Replace("\n\n", ",")
                 .Replace("\n", "")
                 .Split(",")
-                .Tap(numericString => Logger.LogInformation(numericString))
+                .Tap(numericString => Logger.LogInformation("Grid as flat string: {numericString}", numericString))
                 .Select(ConvertStringToBinary)
-                .ToList();
-        if (Program.IsVerbose) {
-            foreach (long keyOrLock in keysAndLocks) {
-                Logger.LogInformation(Convert.ToString(keyOrLock, 2));
+                .Tap(binary => Logger.LogInformation("Binary: {binary}", binary.ToString()))
+                .ForEach(binary => {
+                    if ((binary & 1) == 0) {
+                        Logger.LogInformation("Found Key, binary: {binary}", binary.ToString());
+                        keys.Add(binary);
+                    } else {
+                        Logger.LogInformation("Found Lock, binary: {binary}", binary.ToString());
+                        locks.Add(binary);
+                    }
+                });
+
+        int count = 0;
+        foreach (long key in keys) {
+            foreach (long lock_ in locks) {
+                Logger.LogInformation("Comparing the following 2:");
+                Logger.LogInformation("  --> Lock: {lock}", ToBinaryString(lock_));
+                Logger.LogInformation("  -->  Key: {key}", ToBinaryString(key));
+                bool overlap = (key & lock_) > 0;
+                if (!overlap) {
+                    Logger.LogInformation("  --> Found one!");
+                    count++;
+                }
             }
         }
-        throw new NotImplementedException();
+        return count.ToString();
     }
+
+    private static string ToBinaryString(long keyOrLock) => Convert
+            .ToString(keyOrLock, 2)
+            .PadLeft(35, '0');
 
     private static long ConvertStringToBinary(string input) {
         long result = 0;
         foreach (char c in input) {
             result <<= 1;
-            if (c == '#') {
-                result |= 1;
-            }
+            result |= (uint)(c - '0');
         }
         return result;
     }
