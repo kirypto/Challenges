f0a31734451303203d6f77d582317173ff18f87e
kirypto <kirypto@gmail.com>
2024-12-26 12:40:16 -0600

    AOC-2024 [speed run] Working on day 15, est 40%

--- AdventOfCode/2024/DailyPrograms/Day15.cs
+++ AdventOfCode/2024/DailyPrograms/Day15.cs
@@ -5,6 +5,7 @@ using kirypto.AdventOfCode.Common.Collections.Extensions;
 using kirypto.AdventOfCode.Common.Models;
 using kirypto.AdventOfCode.Common.Repositories;
 using Microsoft.Extensions.Logging;
+using static kirypto.AdventOfCode.Common.Models.CompassDirection;
 
 namespace kirypto.AdventOfCode._2024.DailyPrograms;
 
@@ -24,11 +25,26 @@ public class Day15 : IDailyProgram {
 
         string allMovementInstructions = inputRepository
                 .WithFormatter(raw => raw.Split("\n\n")[1])
+                .WithFormatter(raw => raw.Replace("&lt;", "<").Replace("&gt;", ">"))
                 .FetchLines()
                 .Aggregate((line1, line2) => line1 + line2);
+        Logger.LogInformation("Movement instructions: {instructions}", allMovementInstructions);
 
         Logger.LogInformation("Robot position {coord}", robotPos);
         map.Print();
+
+        foreach (CompassDirection direction in allMovementInstructions.Select(ToCompassDirection)) {
+            robotPos = robotPos.Move(direction);
+            Logger.LogInformation("Robot moved {direction} to {coord}", direction, robotPos);
+        }
         throw new NotImplementedException();
     }
+
+    private static CompassDirection ToCompassDirection(char c) => c switch {
+            '^' => North,
+            '>' => East,
+            'v' => South,
+            '<' => West,
+            _ => throw new InvalidOperationException($"Char {c} is not a valid direction."),
+    };
 }
