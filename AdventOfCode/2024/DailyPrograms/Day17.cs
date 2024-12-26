e2bd139bd035b9c89f6b19247621021d36d2fb02
kirypto <kirypto@gmail.com>
2024-12-26 12:52:06 -0600

    AOC-2024 [speed run] Working on day 17, est 15%

--- AdventOfCode/2024/DailyPrograms/Day17.cs
+++ AdventOfCode/2024/DailyPrograms/Day17.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using kirypto.AdventOfCode.Common.AOC;
 using kirypto.AdventOfCode.Common.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace kirypto.AdventOfCode._2024.DailyPrograms;
 
@@ -13,12 +14,26 @@ public class Day17: IDailyProgram {
         string[] registersAndProgram = inputRepository.Fetch()
                 .Replace(" ", "")
                 .Replace("Register", "")
-                .Replace("Program ", "")
+                .Replace("Program", "")
                 .Split("\n\n");
         Dictionary<string,int> registers = registersAndProgram[0]
                 .Split("\n")
                 .Select(line => line.Split(":"))
                 .ToDictionary(pair => pair[0], nameAndValue => int.Parse(nameAndValue[1]));
+
+
+        IEnumerator<int> program = registersAndProgram[1][1..] // Drop initial ':'
+                .Split(",")
+                .Select(int.Parse)
+                .GetEnumerator();
+        while (program.MoveNext()) {
+            int opcode = program.Current;
+            if (!program.MoveNext()) {
+                throw new InvalidOperationException("Unexpected end of program");
+            }
+            int operand = program.Current;
+            Logger.LogInformation("Performing {opcode} with {operand}", opcode, operand);
+        }
 
         throw new NotImplementedException();
     }
