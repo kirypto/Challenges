4f20419341c57005967640d8bcf15132e0f7605c
kirypto <kirypto@gmail.com>
2025-12-05 18:04:28 -0600

    AOC-2025 Day3 * Get the 10s and 1s digits with a little bit of cheating, soon to be fixed

--- AdventOfCode/2025/day3-part1.wat.js
+++ AdventOfCode/2025/day3-part1.wat.js
@@ -2,23 +2,27 @@ $_$ = require(process.env.WAT);
 
 
 
-$$$ = (_) => ((_$_) => ((__, __$, __$_) => {
+$$$ = (_) => ((_$_) => ((__, __$, __$_, $$) => {
     _$_ = {
         $: 0
     }
-    __$ = _[$_$.$.__$(_) - !-[]]
-    __ = $_$.$.$__(_, __$)
+    __$ = _[$_$.$.__$(_) - !-[]]  // Get '\n' (it's the last character of the input)
+    __ = $_$.$.$__(_, __$)        // Split the input at '\n's
 
-    console.log(`Input split by line: ${__}`)
-    __$ = $_$.$.__$(__)
-    $_$.$.$$([] - [], $ => $ < __$, $ => $ + !+[], $ => {
-        __$_ = $_$.$.__$(__[$])
+    __$ = $_$.$.__$(__)  // Get the number of input lines
+
+    $_$.$.$$([] - [], $ => $ < __$, $ => $ + !+[], $ => { // For each line
+        __$_ = $_$.$.__$(__[$])  // Get the length of the input line
         console.log(`Processing Line '${__[$]}'`)
-        console.log("Max:", $_$.$.__$$(__[$]), "  Min:", $_$.$.__$_(__[$]), "  Length:", __$_)
-        console.log("String Slice:", __[$].slice(3, __$_ - 1), '  Wat Slice:', $_$.$._$$_(__[$], 3, __$_ - 1))
-        console.log("Array Slice:", [1, 2, 3, 4, 5].slice(3, __$_ - 1), '  Wat Slice:', $_$.$._$$_([1, 2, 3, 4, 5], 3, __$_ - 1))
-        console.log("Slice Empty; String: ", $_$.$._$$_(''), "Array:", $_$.$._$$_([]))
-        console.log()
+        $$ = {
+            __: [][[]],  // Placeholder for max char except last (10s digit)
+            $$: [][[]],  // Placeholder for index of 10s digit
+            _: [][[]],   // Placeholder for max char from 10s digit on
+        }
+        $$.__ = $_$.$.__$$($_$.$._$$_(__[$], []-[], __$_-!-[])) // Get the 10s digit
+        $$.$$ = __[$].indexOf($$.__) // I need to write an indexOf method...
+        $$._ = $_$.$.__$$($_$.$._$$_(__[$], $$.$$+!+[], __$_)) // Get the 1s digit
+        console.log($$)
     })
 })() || _$_.$)()
 
